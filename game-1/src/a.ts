ceca4b820e08a1e044ad31b85cae8ad2c6fd8859
liaojunhao <460200123@qq.com>
2024-12-04 20:43:17 +0800

    save3

--- game-1/src/a.ts
+++
@@ -1,1 +0,0 @@
-export const abc: string = '456';
--- game-1/src/index.ts
+++ game-1/src/index.ts
@@ -1,3 +1,19 @@
-import { abc } from './a';
+import { Square } from './core/Square';
+import { SquareGroup } from './core/SquareGroup';
+import { createTeris } from './core/Teris';
+import { TerisRule } from './core/TerisRule';
+import { MoveDirection } from './core/types';
+import { SquarePageViewer } from './core/viewer/SquarePageViewer';
+import $ from 'jquery';
 
-console.log(abc);
+const teris = createTeris({ x: 3, y: 2 });
+console.log('teris', teris);
+
+teris.squares.forEach((sq) => {
+  sq.viewer = new SquarePageViewer(sq, $('#root'));
+});
+
+$('#btnDown').click(function () {
+  //更改中心点坐标
+  TerisRule.moveDirectly(teris, MoveDirection.down);
+});
